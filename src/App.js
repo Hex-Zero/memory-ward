ab2ba61b8cbba3c8e3514eb706a6a81bced4cfb9
Maxwell <hex0@live.com>
2021-11-02 21:41:05 +0000

    add image arrray

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const imageArray = [
+  { src: 1 },
+  { src: 2 },
+  { src: 3 },
+  { src: 4 },
+  { src: 5 },
+  { src: 6 },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
