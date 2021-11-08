6ef71580c6d2c5978bf8bcc331558f5d72ecd6b2
Maxwell <hex0@live.com>
2021-11-08 01:30:15 +0000

    and typescript, new shuffle (O)n and map elements

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function shuffle(array: cardModel[]) {
+  var m = array.length,
+    t,
+    i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+interface cardModel {
+  src : number
+}
+
+function App() {
+  const imageArray: cardModel[] = [
+    { 'src': 1 },
+    { 'src': 2 },
+    { 'src': 3 },
+    { 'src': 4 },
+    { 'src': 5 },
+    { 'src': 6 },
+  ];
+
+  const [cards, setCards] = useState(shuffle([...imageArray, ...imageArray]));
+  const [flipped, setFlipped] = useState([]);
+
+  return <div className="App">
+    <div className="card-grid">
+    {cards.map((element,index) => (
+      
+      <div id={index.toString()} className="card">{element.src}</div>
+    ))}
+    </div>
+
+
+  </div>;
+}
+
+export default App;
